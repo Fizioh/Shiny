86f959b83f6e2e7ccdf8d6e8141a718ec9eb306b
Fizioh <batrom@laposte.net>
2022-03-04 08:55:27 +0100

    complete style according to figma

--- src/pages/Freelances/index.jsx
+++ src/pages/Freelances/index.jsx
@@ -1,5 +1,26 @@
 import Card from '../../components/Card'
 import styled from 'styled-components'
+import colors from '../../utils/style/colors'
+
+const CardsContainer = styled.div`
+    display: grid;
+    gap: 24px;
+    grid-template-rows: 250px 250px;
+    grid-template-columns: repeat(3, 1fr);
+`
+const PageTitle = styled.h1`
+    font-size: 30px;
+    color: black;
+    text-align: center;
+    padding-bottom: 30px;
+`
+const PageSubtitle = styled.h2`
+    font-size: 20px;
+    color: ${colors.secondary};
+    font-weight: 300;
+    text-align: center;
+    padding-bottom: 30px;
+`
 
 const freelanceProfiles = [
     {
@@ -16,17 +37,14 @@ const freelanceProfiles = [
     },
 ]
 
-const CardsContainer = styled.div`
-    display: grid;
-    gap: 24px;
-    grid-template-rows: 250px 250px;
-    grid-template-columns: repeat(3, 1fr);
-`
 
 function Freelances() {
     return (
         <div>
-            <h1>Voici les freelances : </h1>
+            <PageTitle>Trouvez votre prestataire</PageTitle>
+            <PageSubtitle>
+                Chez Shiny nous réunisssons les meilleurs profils pour vous.
+            </PageSubtitle>
             <CardsContainer>
                 {freelanceProfiles.map((profile, index) => (
                     <Card
