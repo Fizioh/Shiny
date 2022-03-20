188517ed8c97b5eabb67c54b235a3afbe2642aec
Fizioh <batrom@laposte.net>
2022-03-20 10:36:18 +0100

    change GlobalStyle to function & finish Dark theme feature

--- src/index.jsx
+++ src/index.jsx
@@ -7,18 +7,15 @@ import Freelances from './pages/Freelances'
 import Results from './pages/Results'
 import Header from './components/Header'
 import Error from './components/Error'
-import {createGlobalStyle} from 'styled-components'
+import ThemeProvider from './utils/context'
+import GlobalStyle from './utils/style/GlobalStyle'
 
-const GlobalStyle = createGlobalStyle`
-    * {
-        font-family: 'Trebuchet MS', Helvetica, sans-serif;
-    }
-`
 
 ReactDOM.render(
     <React.StrictMode>
         <Router>
-            <GlobalStyle />
+            <ThemeProvider>
+                <GlobalStyle />
                 <Header />
                 <Routes>
                     <Route path="/" element={<Home />} />
@@ -27,6 +24,7 @@ ReactDOM.render(
                     <Route path="results" element={<Results />} />
                     <Route path="*" element={<Error />} />
                 </Routes>
+            </ThemeProvider>
         </Router>
     </React.StrictMode>,
     document.getElementById('root')
--- src/utils/context/index.jsx
+++ src/utils/context/index.jsx
@@ -1,5 +1,6 @@
-import { createContext } from 'react';
-import { ThemeContext } from 'styled-components';
+import { createContext, useState } from 'react';
+
+export const ThemeContext = createContext()
 
 export const ThemeProvider = ({children}) => {
     const [theme, setTheme] = useState('light')
